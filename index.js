0b09cbe5f6a20d2a7e3506c48cb3487db6f6211c
kazibilsi <kazibilasi@gmail.com>
2023-05-21 21:32:45 +0600

    modify something

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+const corsOptions ={
+  origin:'*', 
+  credentials:true,
+  optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+
 
 
 
@@ -27,7 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const dollsCollection = client.db('dolls').collection('information')
     const addingCollection = client.db('dolls').collection('addingToys')
@@ -60,12 +69,12 @@ async function run() {
         console.log(result)
       }
     })
-    app.get('/dollsCategory/:subCategory', async (req, res) => {
-      let query = {sub_category: req.params.subCategory }
-      console.log(query)
-      const result = await dollsCollection.find(query).toArray();
-      res.send(result);
-    })
+    // app.get('/dollsCategory/:subCategory', async (req, res) => {
+    //   let query = {sub_category: req.params.subCategory }
+    //   console.log(query)
+    //   const result = await dollsCollection.find(query).toArray();
+    //   res.send(result);
+    // })
 
 
 
@@ -84,8 +93,7 @@ run().catch(console.dir);
 
 
 
-app.use(cors())
-app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.send('Server running!');
